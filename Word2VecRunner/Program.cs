03335534ba92edac173f4c2d66cd5d41e4dfc738
BenSandeen <benjaminsandeen2016@u.northwestern.edu>
2017-02-20 21:50:49 -0700

    first commit

---
+++ Word2VecRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Word2Vec;
+
+namespace Word2VecRunner
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			var builder = Word2Vec.Net.Word2VecBuilder.Create();
+		}
+	}
+}
---
+++ Word2VecUnity/Assets/Word2Vec.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Word2Vec.Net;
+//using Word2Vec;
+
+public class Word2Vec : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Distance d = new Distance("potato");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
